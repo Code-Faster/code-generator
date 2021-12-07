5ee70a65a4609645f610d77e0ec69ee2e124d617
libiqi <libiqi@createsz.cn>
2021-12-07 15:13:06 +0800

    build: bundle 0.0.1

--- dist/code-generator.common.js
+++ dist/code-generator.common.js
@@ -5,14 +5,41 @@
   */
 'use strict';
 
-function greeter(person) {
-  return "Hello, " + person.firstName + " " + person.lastName;
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
 }
 
-var user = {
-  firstName: "Jane",
-  lastName: "User"
-};
-var index = greeter(user);
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
 
-module.exports = index;
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var CodeGenerator = /*#__PURE__*/function () {
+  function CodeGenerator(options) {
+    _classCallCheck(this, CodeGenerator);
+  }
+
+  _createClass(CodeGenerator, [{
+    key: "generator",
+    value: function generator(template) {
+      return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+    }
+  }]);
+
+  return CodeGenerator;
+}();
+
+module.exports = CodeGenerator;
--- dist/code-generator.js
+++ dist/code-generator.js
@@ -4,21 +4,48 @@
   * @license Apache-2.0
   */
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["code-generator"] = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["code-generator"] = factory());
 })(this, (function () { 'use strict';
 
-    function greeter(person) {
-      return "Hello, " + person.firstName + " " + person.lastName;
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  var CodeGenerator = /*#__PURE__*/function () {
+    function CodeGenerator(options) {
+      _classCallCheck(this, CodeGenerator);
     }
 
-    var user = {
-      firstName: "Jane",
-      lastName: "User"
-    };
-    var index = greeter(user);
+    _createClass(CodeGenerator, [{
+      key: "generator",
+      value: function generator(template) {
+        return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+      }
+    }]);
 
-    return index;
+    return CodeGenerator;
+  }();
+
+  return CodeGenerator;
 
 }));
