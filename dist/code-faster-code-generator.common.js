8048e2036e69f31e2b6fdd779ee833030ca3a71f
libiqi <libiqi@createsz.cn>
2021-12-09 01:24:37 +0800

    build: bundle 0.0.7

--- dist/code-faster-code-generator.common.js
+++ dist/code-faster-code-generator.common.js
@@ -1,14 +1,46 @@
 /*!
-  * code-generator v0.0.6
+  * code-generator v0.0.7
   * (c) 2021 biqi li
   * @license Apache-2.0
   */
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-function generator(template) {
-  return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
 }
 
-exports.generator = generator;
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var CodeGenerator = /*#__PURE__*/function () {
+  function CodeGenerator() {
+
+    _classCallCheck(this, CodeGenerator);
+  }
+
+  _createClass(CodeGenerator, [{
+    key: "generator",
+    value: function generator(template) {
+      return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+    }
+  }]);
+
+  return CodeGenerator;
+}();
+
+module.exports = CodeGenerator;
--- dist/code-faster-code-generator.js
+++ dist/code-faster-code-generator.js
@@ -1,20 +1,52 @@
 /*!
-  * code-generator v0.0.6
+  * code-generator v0.0.7
   * (c) 2021 biqi li
   * @license Apache-2.0
   */
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["code-faster-code-generator"] = {}));
-})(this, (function (exports) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["code-faster-code-generator"] = factory());
+})(this, (function () { 'use strict';
 
-    function generator(template) {
-      return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  var CodeGenerator = /*#__PURE__*/function () {
+    function CodeGenerator() {
+
+      _classCallCheck(this, CodeGenerator);
     }
 
-    exports.generator = generator;
+    _createClass(CodeGenerator, [{
+      key: "generator",
+      value: function generator(template) {
+        return "get template name  " + template.name + " , language is " + template.language + " , will copy from :" + template.fromPath + "  to :" + template.toPath;
+      }
+    }]);
 
-    Object.defineProperty(exports, '__esModule', { value: true });
+    return CodeGenerator;
+  }();
+
+  return CodeGenerator;
 
 }));
